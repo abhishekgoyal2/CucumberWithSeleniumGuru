8d0301ba77794ed0c0e3aee96eedd07d025054f6
abhishek.goyal2 <abhi25goel@gmail.com>
2018-09-05 17:37:40 +0530

    commit change to feature file

--- Features/MyTest.feature
+++ Features/MyTest.feature
@@ -1,13 +1,13 @@
-Feature: Reset functionality on login page of Application 
+Feature: Reset functionality on login page of Application
 
+  Scenario Outline: Verification of reset button with numbers of credential
+    Given Open the Firefox and launch the application
+    When Enter the Username <username>and Password <password>
+    Then Reset the credential
+    #Then close the browser
 
-Scenario: Verification of Reset button 
-
-
-Given Open the Firefox and launch the application			
-
-
-When Enter the Username and Password			
-
-
-Then Reset the credential	
+    Examples: 
+      | username | password  |
+      | User1    | password1 |
+      | User2    | password2 |
+      | User3    | password3 |
--- src/StepDefinition/Steps.java
+++ src/StepDefinition/Steps.java
@@ -1,29 +1,48 @@
 package StepDefinition;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Steps {
 
-	
+	WebDriver driver;
 	 @Given("^Open the Firefox and launch the application$")				
 	    public void open_the_Firefox_and_launch_the_application() throws Throwable							
 	    {		
-	        System.out.println("This Step open the Firefox and launch the application.");					
+	        System.out.println("This Step open the Firefox and launch the application.");	
+		 driver= new FirefoxDriver();					
+	       driver.manage().window().maximize();			
+	       driver.get("http://demo.guru99.com/v4");	
+					
 	    }		
 
-	    @When("^Enter the Username and Password$")					
+	 @When("^Enter the Username User(\\d+)and Password password(\\d+)$")				
 	    public void enter_the_Username_and_Password() throws Throwable 							
 	    {		
-	       System.out.println("This step enter the Username and Password on the login page.");					
+	       System.out.println("This step enter the Username and Password on the login page.");	
+	       driver.findElement(By.name("uid")).sendKeys("username");							
+	       driver.findElement(By.name("password")).sendKeys("password");	
 	    }		
 
 	    @Then("^Reset the credential$")					
 	    public void Reset_the_credential() throws Throwable 							
 	    {    		
-	        System.out.println("This step click on the Reset button.");					
+	        System.out.println("This step click on the Reset button.");	
+	        driver.findElement(By.name("btnReset")).click();		
 	    }		
+
+//	    @Then("^close the browser$")
+//	    public void close_the_browser() throws Throwable {
+//	        // Write code here that turns the phrase above into concrete actions
+//	    	driver.quit();
+////	        throw new PendingException();
+//	    }
 
 	}
 	
